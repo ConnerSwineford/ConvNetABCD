d2f1469d7567fdfabfea9be56f3d3f2625ecb135
ConnerSwineford <cswineford@sdsu.edu>
2024-06-21 18:21:32 -0700

    updating source for readability

--- source/model.py
+++ source/model.py
@@ -1,7 +1,5 @@
 import torch
 import numpy as np
-#import matplotlib.pyplot as plt
-from tqdm import tqdm
 import torch.nn as nn
 from torch.optim import Adam
 import torch.nn.functional as F
@@ -20,61 +18,6 @@ __license__ = 'MIT'
 ## Authors: Conner Swineford and Johanna Walker
 ## License: MIT License
 ############################################################################
-
-def train_loop(dataloader, model, loss_fn, optimizer, rank=0):
-  """
-  Training loop for the model.
-
-  Args:
-    dataloader (torch.utils.data.DataLoader): The dataloader for the training data.
-    model (torch.nn.Module): The model to train.
-    loss_fn (torch.nn.Module): The loss function.
-    optimizer (torch.optim.Optimizer): The optimizer.
-    device (torch.device, optional): The device to use for training. Defaults to HPU.
-
-  Returns:
-    float: The average training loss.
-  """
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-  train_loss = 0.
-  for i, (X, y, _) in enumerate(tqdm(dataloader)):
-    # Compute prediction and loss
-    optimizer.zero_grad()  # zero_grad()
-    pred = model(X.float().to(device)) # Get the prediction output from model
-    loss = loss_fn(pred, torch.autograd.Variable(utils.code_targets(y).float().to(device), requires_grad=True))  # compute loss by calling loss_fn()
-    # Backpropagation
-    loss.backward()  # backward()
-    optimizer.step()  # step()
-    train_loss += int(loss.item())
-
-  return train_loss / len(dataloader) # average loss within this epoch
-
-
-@torch.no_grad()
-def val_loop(dataloader, model, loss_fn):
-  """
-  Validation loop for the model.
-
-  Args:
-    dataloader (torch.utils.data.DataLoader): The dataloader for the validation data.
-    model (torch.nn.Module): The model to validate.
-    loss_fn (torch.nn.Module): The loss function.
-    device (torch.device, optional): The device to use for validation. Defaults to HPU.
-
-  Returns:
-    float: The average validation loss.
-  """
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-  val_loss = 0.
-  for X, y, _ in dataloader:
-    # Compute prediction and loss
-    pred = model(X.float().to(device)) # Get the prediction output from model
-    val_loss += float(loss_fn(pred, utils.code_targets(y).float()).item()) # compute loss
-
-  return val_loss / len(dataloader) # average loss of the validation set 
-
 
 class ConvBlock(nn.Module):
   """
@@ -154,10 +97,9 @@ class ConvNet(nn.Module):
   """
   def __init__(self, dims):
     super(ConvNet, self).__init__()
-    self.dims = dims
+    
     self.gradients = None
     self.activations = None
-    self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     self.conv1 = ConvBlock(1, 8, kernel_size=5, dropout=0.2)
     self.conv2 = ConvBlock(8, 8, kernel_size=5)
@@ -221,75 +163,4 @@ class ConvNet(nn.Module):
     fw = self.conv7.conv.register_forward_hook(self.activations_hook)
 
     return x
-
-  def train(self, loader, epochs=10, loss_fn=nn.BCEWithLogitsLoss(), optimizer=Adam, learning_rate=0.00001, graph=True,
-            seed=None, outdir='./', rank=0):
-    """
-    Train the CNN model.
-
-    Args:
-      loader (torch.utils.data.DataLoader): DataLoader for the training data.
-      epochs (int, optional): Number of epochs to train. Defaults to 10.
-      loss_fn (torch.nn.Module, optional): Loss function. Defaults to nn.BCEWithLogitsLoss().
-      optimizer (torch.optim.Optimizer, optional): Optimizer. Defaults to Adam.
-      learning_rate (float, optional): Learning rate. Defaults to 0.00001.
-      graph (bool, optional): Whether to plot loss graphs. Defaults to True.
-      seed (int, optional): Random seed for reproducibility. Defaults to None.
-      outdir (str, optional): Directory to save the model and loss values. Defaults to './'.
-      rank (int, optional): Rank of the process in distributed training. Defaults to 0.
-    """
-    train_loss, val_loss = np.zeros(epochs + 1), np.zeros(epochs + 1)
-
-    if type(seed) == int: torch.manual_seed(seed)
-    optimizer = optimizer(params=self.parameters(), lr=learning_rate)
-
-    best_loss = float('inf')
-    for t in range(epochs):
-      if rank==0:
-        print(f'Epoch {t+1}:', end=' ')
-
-      train_loss[t] = train_loop(loader, self, loss_fn, optimizer, rank=rank)
-      
-      if rank == 0:  
-        val_loss[t] = val_loop(loader, self, loss_fn)
-        print(f' Training Loss: {train_loss[t]:.5}, Validation Loss: {val_loss[t]:.5}')
-        if val_loss[t] < best_loss:
-          print(f' Validation loss improved from {best_loss:.5} to {val_loss[t]:.5}. Saving current model...')
-          best_loss = val_loss[t]
-          torch.save(self.state_dict(), os.path.join(outdir, 'model_weights.pth'), pickle_protocol=4)
-        else:
-          print(f' Validation loss did not improve.')
-
-      torch.distributed.barrier()
-
-    if rank == 0:
-      self.load_state_dict(torch.load(os.path.join(outdir, 'model_weights.pth')))
-      if graph:
-        np.save(os.path.join(outdir, 'train_loss.npy'), train_loss)
-        np.save(os.path.join(outdir, 'val_loss.npy'), val_loss)
-
-
-  def HiResRAM(self, loader, loss_fn=nn.MSELoss(), rank=0, device='cpu'):
-    """
-    Computes high-resolution region activation mapping (RAM).
-        
-    Args:
-      loader (torch.utils.data.DataLoader): The data loader.
-      loss_fn (callable): The loss function.
-      rank (int): Rank of the process for distributed training.
-        
-    Returns:
-      np.ndarray: The RAM values.
-    """
-    m = np.empty((0, self.dims['height'], self.dims['width'], self.dims['depth']))
-    #for X_batch, Y_batch, _ in enumerate(tqdm(loader)):
-    for X_batch, Y_batch, _ in tqdm(loader):
-      X_batch = X_batch.to(self.device)
-      Y_batch = Y_batch.to(self.device)
-      pred = self(X_batch)
-      loss_fn(pred, utils.code_targets(Y_batch.float()).to(self.device)).backward()
-      if self.activations is not None:
-          m = np.concatenate((m, utils.resize_image_3d((self.activations.detach().cpu().numpy()*self.gradients.detach().cpu().numpy()).sum(1))), 0)
-
-    return torch.Tensor(m)
 
--- source/train.py
+++ source/train.py
@@ -1,26 +1,27 @@
 import os
-import model
-import utils
 import argparse
-import time
-import pickle
 import torch
+import torch.nn as nn
+import torch.optim as optim
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch.utils.data import DataLoader
-from torch.utils.data.dataloader import default_collate
 from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.optim as optim
+from torch import save
+from tqdm import tqdm
+import numpy as np
 import nibabel as nib
+import time
 
+import model
+import utils
 
 # Argument parser setup for command line arguments
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Train a 3D CNN model.")
 parser.add_argument('--data_path', type=str, help='Path to the csv of the training data')
 parser.add_argument('--batch_size', default=16, type=int, help='Number of subjects in one model iteration')
 parser.add_argument('--epochs', default=10, type=int, help='Number of iterations through the entire data set')
 parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate')
-parser.add_argument('--affine', default='./affine.npy', type=str, help='Path to the saved affine numpy array')
 parser.add_argument('--outdir', default='./', type=str, help='File path to dump script output')
 parser.add_argument('--workers', type=int, default=1, help='Number of processes for multiprocessing')
 parser.add_argument('--weights', default=None, type=str,
@@ -28,58 +29,146 @@ parser.add_argument('--weights', default=None, type=str,
 parser.add_argument('--seed', default=None, help='(Optional) Random seed for replicability')
 args = parser.parse_args()
 
+def train_loop(dataloader, model, loss_fn, optimizer, epoch, rank=0):
+    """
+    Training loop for the model.
+
+    Args:
+        dataloader (torch.utils.data.DataLoader): The dataloader for the training data.
+        model (torch.nn.Module): The model to train.
+        loss_fn (torch.nn.Module): The loss function.
+        optimizer (torch.optim.Optimizer): The optimizer.
+        device (torch.device, optional): The device to use for training. Defaults to HPU.
+
+    Returns:
+        float: The average training loss.
+  """
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_loss = 0.
+    for i, (X, y, _) in enumerate(tqdm(dataloader, leave=False, desc=f'Training Epoch {epoch+1}')):
+        # Compute prediction and loss
+        optimizer.zero_grad()  # zero_grad()
+        pred = model(X.float().to(device)) # Get the prediction output from model
+        loss = loss_fn(pred, torch.autograd.Variable(utils.code_targets(y).float().to(device), requires_grad=True))  # compute loss by calling loss_fn()
+        # Backpropagation
+        loss.backward()  # backward()
+        optimizer.step()  # step()
+        train_loss += int(loss.item())
+
+    return train_loss / len(dataloader) # average loss within this epoch
+
+@torch.no_grad()
+def val_loop(dataloader, model, loss_fn):
+    """
+    Validation loop for the model.
+
+    Args:
+        dataloader (torch.utils.data.DataLoader): The dataloader for the validation data.
+        model (torch.nn.Module): The model to validate.
+        loss_fn (torch.nn.Module): The loss function.
+        device (torch.device, optional): The device to use for validation. Defaults to HPU.
+
+    Returns:
+        float: The average validation loss.
+    """
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    val_loss = 0.
+    for X, y, _ in dataloader:
+        # Compute prediction and loss
+        pred = model(X.float().to(device)) # Get the prediction output from model
+        val_loss += float(loss_fn(pred, utils.code_targets(y).float()).item()) # compute loss
+
+    return val_loss / len(dataloader) # average loss of the validation set
+
+def train(model, loader, epochs, learning_rate, outdir, seed, graph=True, loss_fn=nn.BCEWithLogitsLoss(), optimizer=optim.Adam, rank=0):
+    """
+    Train the CNN model.
+
+    Args:
+        loader (torch.utils.data.DataLoader): DataLoader for the training data.
+        epochs (int, optional): Number of epochs to train. Defaults to 10.
+        loss_fn (torch.nn.Module, optional): Loss function. Defaults to nn.BCEWithLogitsLoss().
+        optimizer (torch.optim.Optimizer, optional): Optimizer. Defaults to Adam.
+        learning_rate (float, optional): Learning rate. Defaults to 0.00001.
+        graph (bool, optional): Whether to plot loss graphs. Defaults to True.
+        seed (int, optional): Random seed for reproducibility. Defaults to None.
+        outdir (str, optional): Directory to save the model and loss values. Defaults to './'.
+        rank (int, optional): Rank of the process in distributed training. Defaults to 0.
+    """
+    train_loss, val_loss = np.zeros(epochs + 1), np.zeros(epochs + 1)
+
+    if type(seed) == int: torch.manual_seed(seed)
+    optimizer = optimizer(params=model.parameters(), lr=learning_rate)
+
+    best_loss = float('inf')
+    for t in range(epochs):
+        if rank==0:
+            print(f'Epoch {t+1}:', end=' ')
+
+        train_loss[t] = train_loop(loader, model, loss_fn, optimizer, t, rank=rank)
+
+        if rank == 0:
+            val_loss[t] = val_loop(loader, model, loss_fn)
+            print(f' Training Loss: {train_loss[t]:.5}, Validation Loss: {val_loss[t]:.5}')
+            if val_loss[t] < best_loss:
+                print(f' Validation loss improved from {best_loss:.5} to {val_loss[t]:.5}. Saving current model...')
+                best_loss = val_loss[t]
+                torch.save(model.state_dict(), os.path.join(outdir, 'model_weights.pth'), pickle_protocol=4)
+            else:
+                print(f' Validation loss did not improve.')
+
+        torch.distributed.barrier()
+
+    if rank == 0:
+        model.load_state_dict(torch.load(os.path.join(outdir, 'model_weights.pth')))
+        if graph:
+            np.save(os.path.join(outdir, 'train_loss.npy'), train_loss)
+            np.save(os.path.join(outdir, 'val_loss.npy'), val_loss)
 
 # Define the training function
-def train(rank, world_size):
+def main(rank, n_workers, device):
 
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-  # Set up the process group
-  dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    # Set up the process group
+    dist.init_process_group("gloo", rank=rank, world_size=n_workers)
 
-  print(f"Rank {rank} Initialized")
+    # Import and prepare training data
+    df = utils.import_raw_data(args.data_path) 
+    data = utils.NiiDataset(df['NiiPath'], df['Irr'], df['SubjID'])
+    sampler = torch.utils.data.distributed.DistributedSampler(data)
+    loader = DataLoader(data, batch_size=args.batch_size, num_workers=0, sampler=sampler)
 
-  # Import and prepare training data
-  train = utils.import_raw_data(args.data_path) 
-  train_data = utils.NiiDataset(train['NiiPath'], train['Irr'], train['SubjID'])
-  train_sampler = torch.utils.data.distributed.DistributedSampler(train_data)
-  train_loader = DataLoader(train_data, batch_size=args.batch_size, num_workers=0, sampler=train_sampler)
-
-  print(f'Data Import Complete [{rank}]')
-
-  # Initialize model and get input dimensions
-  dims = utils.get_loader_dims3d(train_loader)
+    # Initialize model and get input dimensions
+    dims = utils.get_loader_dims3d(loader)
     
-  # Create the model
-  CNN = model.ConvNet(dims).float().to(device)
-  ddp_model = DDP(CNN)
-    
-  print(f'Model Initialized [{rank}]')
+    # Create the model
+    CNN = model.ConvNet(dims).float().to(device)
+    ddp_model = DDP(CNN)
   
-  if rank == 0:
-    start = time.time()
+    if rank == 0:
+        start = time.time()
 
-  torch.distributed.barrier()  
+    torch.distributed.barrier()  
 
-  CNN.train(train_loader, epochs=args.epochs, learning_rate=args.lr, graph=True, seed=args.seed, outdir=args.outdir, rank=rank)
+    train(CNN, loader, epochs=args.epochs, learning_rate=args.lr, outdir=args.outdir, seed=args.seed, graph=True, rank=rank)
 
-  if rank == 0:
-    print('Training Time:', time.time()-start)
+    if rank == 0:
+        print('Training Time:', time.time()-start)
 
-  # Clean up
-  dist.destroy_process_group()
+    # Clean up
+    dist.destroy_process_group()
 
 if __name__ == "__main__":
-
-  # Number of processes to use
-  world_size = args.workers
    
-  # Set environment variables for master address and port
-  os.environ['MASTER_ADDR'] = 'localhost'
-  os.environ['MASTER_PORT'] = '12355'
+    # Set environment variables for master address and port
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
  
-  #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-  # Use multiprocessing to spawn processes
-  mp.spawn(train, args=(world_size,), nprocs=world_size, join=True)
+    # Use multiprocessing to spawn processes
+    mp.spawn(main, args=(args.workers, device,), nprocs=args.workers, join=True)
 
